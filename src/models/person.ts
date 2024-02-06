accc183e985190dfe74d8ce1e34504409b9f4a67
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-06 14:50:53 +0800

    created a person schema model

---
+++ src/models/person.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { NewPerson } from "../types/Person";
+
+const personSchema = new mongoose.Schema<NewPerson>({
+  name: {
+    type: String,
+    required: true
+  },
+  number: String
+});
+
+export default mongoose.model<NewPerson>('Person', personSchema);
