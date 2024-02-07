737cf2b2cc0eaecb1068536b0f729b463280d8b6
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-07 16:11:18 +0800

    create service to GET a specific person

--- src/controllers/persons.ts
+++ src/controllers/persons.ts
@@ -7,3 +7,12 @@ const getAllPersons = (_req: Request, res: Response, next: NextFunction) => {
     .then(persons => res.json(persons))
     .catch(error => next(error));
 };
+
+// Get a specific person based on given id
+const getPerson = (req: Request, res: Response, next: NextFunction) => {
+  PersonModel.findById(req.params.id)
+    .then(person => {
+      person ? res.json(person).end() : res.status(404).json({ error : 'Cannot find note with that id'}).end()
+    })
+    .catch(error => next(error));
+};
