05982ba6bf14319ba36205cc6808c55d285192b3
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-09 15:56:06 +0800

    type guard and narrowing moved to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,34 @@
+import { NewPerson } from '../types/person';
+
 // String type guard
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
 };
 
-export {
-  isString
+// String parser
+const parseString = (str: unknown): string => {
+  if (!isString(str)) {
+    throw new Error(`Invalid input: ${str}`);
+  }
+  return str;
 };
+
+// toNewPerson
+const toNewPerson = (object: unknown): NewPerson => {
+  if (!object || typeof object !== 'object') {
+    throw new Error('Incorrect or missing data!');
+  }
+
+  if ('name' in object && 'number' in object) {
+    const newPerson = {
+      name: parseString(object.name),
+      number: parseString(object.number),
+    };
+
+    return newPerson;
+  }
+
+  throw new Error('Invalid input data!');
+};
+
+export default toNewPerson;
