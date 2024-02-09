d9f63b64c7bfbb63fef0624f2decabd8996c9939
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-09 15:55:46 +0800

    use ExpressParams for typing request parameter types

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,6 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request } from 'express';
 import morgan from 'morgan';
 import logger from './logger';
+import { ExpressParams } from '../types/expressParams';
 
 // Morgan middleware
 morgan.token('body', (req: Request, _res) => {
@@ -11,14 +12,14 @@ const morganCustomTokens = ':method :url :status :res[content-length] - :respons
 const customMorgan = morgan(morganCustomTokens);
 
 // Catch requests to non-existing routes
-const unknownEndpoint = (_req: Request, res: Response) => {
+const unknownEndpoint = ({res}: ExpressParams) => {
   return res.status(404).send({
     error: 'unknown endpoint'
   });
 };
 
 // Error handler
-const errorHandler = (error: Error, _req: Request, res: Response, next: NextFunction) => {
+const errorHandler = ({error, res, next}: ExpressParams) => {
   logger.error(error.message);
 
   switch (error.name) {
