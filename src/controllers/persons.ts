642264e019a4c8d20eca1b58addadd97b4b08cf8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-10 08:29:03 +0800

    manually define req res types instead of using ExpressParams

--- src/controllers/persons.ts
+++ src/controllers/persons.ts
@@ -1,16 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
 import PersonModel from '../models/person';
-import { ExpressParams } from '../types/expressParams';
 import toNewPerson from '../utils/utils';
 
 // GET all persons
-const getAllPersons = ({res, next}: ExpressParams) => {
+const getAllPersons = (_req: Request, res: Response, next: NextFunction) => {
   PersonModel.find({})
     .then(persons => res.json(persons))
     .catch(error => next(error));
 };
 
 // GET a specific person based on given id
-const getPerson = ({req, res, next}: ExpressParams) => {
+const getPerson = (req: Request, res: Response, next: NextFunction) => {
   PersonModel.findById(req.params.id)
     .then(person => {
       person ? res.json(person).end() : res.status(404).json({ error : 'Cannot find note with that id'}).end();
@@ -19,14 +19,14 @@ const getPerson = ({req, res, next}: ExpressParams) => {
 };
 
 // DELETE a specific person based on given id
-const deletePerson = ({req, res, next}: ExpressParams) => {
+const deletePerson = (req: Request, res: Response, next: NextFunction) => {
   PersonModel.findByIdAndDelete(req.params.id)
     .then(() => res.status(204).end())
     .catch(error => next(error));
 };
 
 // UPDATE a specific person
-const updatePerson = ({req, res, next}: ExpressParams) => {
+const updatePerson = (req: Request, res: Response, next: NextFunction) => {
   const {name, number} = toNewPerson(req.body);
 
   const toUpdatePerson = {
@@ -39,7 +39,7 @@ const updatePerson = ({req, res, next}: ExpressParams) => {
 };
 
 // CREATE a person
-const createPerson = ({req, res, next}: ExpressParams) => {
+const createPerson = (req: Request, res: Response, next: NextFunction) => {
   const validatedPerson = toNewPerson(req.body);
 
   const newPerson = new PersonModel({
