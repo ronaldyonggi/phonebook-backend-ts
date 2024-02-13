b6a38f4aba04b3b76b54452fec8a38993fd63e85
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-13 11:40:42 +0800

    linter formatting fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import app from "./app";
-import config from "./utils/config";
-import logger from "./utils/logger";
+import app from './app';
+import config from './utils/config';
+import logger from './utils/logger';
 
 app.listen(config.PORT!, () => {
   logger.info(`Server is running on port ${config.PORT}`);
